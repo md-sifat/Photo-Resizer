e2131d3c0e78974d8e44d7389ba7b874b70edacd
mdsifat <mdsifat7252@gmail.com>
2023-02-03 22:21:17 +0600

    js update

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const clickBox = document.querySelector(".upload"),   //selecting the icon
 inputBox = clickBox.querySelector("input"),    // selecting the input field
-imagePreview = document.querySelector(".image");
-
-
+imagePreview = document.querySelector(".image"),
+widthInput = document.querySelector(".input1 input"),
+heightInput = document.querySelector(".input2 input");
 
 
 const loadFile = (uploadedFiles) =>{
     const file = uploadedFiles.target.files[0];  //it will save the first user selected file
-    if(!file) return;  //id select no file
+    if(!file) return;  //if select no file
     imagePreview.src = URL.createObjectURL(file); 
     console.log(file); // for terminal debug
-
+    imagePreview.addEventListener("load" , ()=>{
+        widthInput.value = imagePreview.naturalWidth;
+        heightInput.value = imagePreview.naturalHeight;
+        document.querySelector(".upload").classList.add("active");
+    });
 }
 
 inputBox.addEventListener("change" , loadFile); 
