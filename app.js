b14e8379f147388cc1f41ac8cdca1d6bcd6434b5
mdsifat <mdsifat7252@gmail.com>
2023-02-03 23:26:35 +0600

    finished up

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ imagePreview = document.querySelector(".image"),
 widthInput = document.querySelector(".input1 input"),
 heightInput = document.querySelector(".input2 input"),
 ratioInput = document.querySelector(".ratio input"),
-downloadBtn  = document.querySelector(".btn");
+downloadBtn  = document.querySelector(".btn"),
+qualityInput = document.querySelector(".quality , input");
 
 let ratio;
 
@@ -31,9 +32,25 @@ heightInput.addEventListener("keyup" , () => {
     widthInput.value =Math.floor(width);
 })
 
+const Download = () => {
+    const canvas = document.createElement("canvas");
+    const a = document.createElement("a");
+    const ctx = canvas.getContext("2d");
+
+    const imageQuality  = qualityInput.checked ? 0.7 : 1.0;
+
+    canvas.width = widthInput.value;
+    canvas.height = heightInput.value;
+
+    ctx.drawImage(imagePreview , 0 ,0 , canvas.width ,canvas.height);
+
+    a.href =canvas.toDataURL("image/jpeg" , imageQuality);
+    a.download = new Date().getTime();
+    a.click();
+}
 
 
-
+downloadBtn.addEventListener("click" , Download)
 
 
 inputBox.addEventListener("change" , loadFile); 
