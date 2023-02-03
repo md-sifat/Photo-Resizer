01fdd9e1e23d88def808898bea217bd02cfa28a9
mdsifat <mdsifat7252@gmail.com>
2023-02-03 22:43:00 +0600

    engine update

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const clickBox = document.querySelector(".upload"),   //selecting the icon
 inputBox = clickBox.querySelector("input"),    // selecting the input field
 imagePreview = document.querySelector(".image"),
 widthInput = document.querySelector(".input1 input"),
-heightInput = document.querySelector(".input2 input");
+heightInput = document.querySelector(".input2 input"),
+ratioInput = document.querySelector(".ratio input"),
+downloadBtn  = document.querySelector(".btn");
 
+let ratio;
 
 const loadFile = (uploadedFiles) =>{
     const file = uploadedFiles.target.files[0];  //it will save the first user selected file
@@ -13,9 +16,25 @@ const loadFile = (uploadedFiles) =>{
     imagePreview.addEventListener("load" , ()=>{
         widthInput.value = imagePreview.naturalWidth;
         heightInput.value = imagePreview.naturalHeight;
+        ratio = imagePreview.naturalWidth / imagePreview.naturalHeight;
         document.querySelector(".upload").classList.add("active");
     });
 }
+
+widthInput.addEventListener("keyup" , () => {
+    const height = ratioInput.checked ? widthInput.value / ratio  :  heightInput.value;
+    heightInput.value =Math.floor(height);
+})
+
+heightInput.addEventListener("keyup" , () => {
+    const width = ratioInput.checked ? heightInput.value * ratio  :  widthInput.value;
+    widthInput.value =Math.floor(width);
+})
+
+
+
+
+
 
 inputBox.addEventListener("change" , loadFile); 
 clickBox.addEventListener("click" , ()=>{    //connection the icon with the input field
